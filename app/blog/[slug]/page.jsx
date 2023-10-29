67d3541b72d7d3eda1659391f8bcd1d8d2263a86
Tanwen <tanwen@endlessclouds.xyz>
2023-10-29 02:02:40 -0300

    fix

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,14 +1,12 @@
 import { Welcome } from '@/components/Welcome/Welcome';
 import { ColorSchemeToggle } from '@/components/ColorSchemeToggle/ColorSchemeToggle';
 
-export const dynamicParams = false;
-
 export async function generateStaticParams() {
   const slugs = ["blog-post-1", "blog-post-2", "blog-post-3"]
-  return slugs.map((slug) => ({ params: { slug } }))
+  return slugs.map((slug) => ({ slug }))
 }
 
-export default function Page() {
+export default function Page({ params }) {
   return (
     <>
       <Welcome />
