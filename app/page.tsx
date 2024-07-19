e7aaac5114d26bac5ab0b6c648b0abb291fbf0ff
DIEGO\diego <diego.braga.guedes@outlook.com>
2024-07-19 10:53:24 -0300

    layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,7 @@
 "use client";
 
 import { Home } from '../components/Home/Home';
-import { Header } from '../components/Header/Header';
-import { AppShell, Burger, AppShellHeader, AppShellMain } from '@mantine/core';
-import { useState } from 'react';
 
 export default function HomePage() {
-  const [opened, setOpened] = useState(false);
-
-  const toggle = () => {
-    setOpened(!opened);
-  }
-
-  return (
-    <AppShell
-      header={{ height: 60 }}
-      navbar={{
-        width: 300,
-        breakpoint: 'sm',
-        collapsed: { mobile: !opened },
-      }}
-      padding="md"
-    >
-      <AppShellHeader>
-        <Header />
-      </AppShellHeader>
-
-      <AppShellMain><Home /></AppShellMain>
-    </AppShell>
-  );
+  return <Home/>
 }
